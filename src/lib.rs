ce1f56c80d7a2242c16203a92629229f609ee97a
Suresh Rangarajulu <surya501@yahoo.com>
2022-09-05 13:58:33 -0700

    test: ✅ First integration test added

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+// health check handler
+// check this using the following command
+// curl -v http://127.0.0.1:8080/health_check
+async fn health() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health)))
+        .bind("127.0.0.1:8000")?
+        .run();
+
+    // look no await here
+    Ok(server)
+}
---
+++ tests/health_check.rs
@@ -0,0 +1,28 @@
+// `tokio::test` is the testing equivalent of `tokio::main`.
+// It also spares you from having to specify the `#[test]` attribute.
+//
+// You can inspect what code gets generated using
+// `cargo expand --test health_check` (<- name of the test file)
+#[tokio::test]
+async fn health_check_works() {
+    // Let's first spawn the app first.
+    spawn_app();
+
+    // Act
+    let address = "http://127.0.0.1:8000";
+    let client = reqwest::Client::new();
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = base_proj::run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+}
