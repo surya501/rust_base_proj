049ba1f1c0862c8364b3c62f24597682853daa6f
Suresh Rangarajulu <surya501@yahoo.com>
2022-09-06 05:48:38 -0700

    build: 🗃️ postgres create and migrate

---
+++ scripts/init_db.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# Run this script like this:
+# $ SKIP_DOCKER=true ./scripts/init_db.sh
+
+set -x
+set -eo pipefail
+
+#check if psql is installed
+if ! [ -x "$(command -v psql)" ]; then
+  echo 'Error: psql is not installed.' >&2
+  exit 1
+fi
+
+# check if sqlx is installed
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo 'Error: sqlx is not installed.' >&2
+  exit 1
+fi
+
+
+#Check if custom user has been set, otherwise use default
+DB_USER=${POSTGRES_USER:=postgres}
+DB_NAME=${POSTGRES_DB:=newsletter}
+DB_PASS=${POSTGRES_PASSWORD:=postgres}
+DB_PORT=${POSTGRES_PORT:=5432}
+
+# Kill the existing docker image if it exists
+docker kill newsletter-db || true
+# Launch postgres using Docker
+docker run --rm --name newsletter-db -e POSTGRES_USER=$DB_USER -e POSTGRES_PASSWORD=$DB_PASS -e POSTGRES_DB=$DB_NAME -p $DB_PORT:5432 -d postgres postgres -N 1000
+# docker run -e POSTGRES_USER=$DB_USER -e POSTGRES_PASSWORD=$DB_PASS -e POSTGRES_DB=$DB_NAME -p $DB_PORT:5432 -d postgres postgres -N 1000
+
+# keep pinging postgres until it's ready to accept commands
+export PGPASSWORD=$DB_PASS
+until psql -h localhost -U $DB_USER -p $DB_PORT -d $DB_NAME -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+
+export DATABASE_URL=postgres://$DB_USER:$DB_PASS@localhost:$DB_PORT/$DB_NAME
+sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
