c90ae0018adf36338581ac917edc7841a7a9afe1
Suresh Rangarajulu <surya501@yahoo.com>
2022-09-06 06:22:28 -0700

    feat: ✨ Add support for reading a config file for applicaiton and db parameters

---
+++ src/configuration.rs
@@ -0,0 +1,25 @@
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseConfiguration,
+    pub application_port: u16,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseConfiguration {
+    pub username: String,
+    pub password: String,
+    pub host: String,
+    pub port: u16,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let settings = config::Config::builder()
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
+        .build()?;
+
+    settings.try_deserialize::<Settings>()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::net::TcpListener;
 
-use base_proj::startup::run;
+use base_proj::{configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    let configuration = configuration::get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind random port");
     // let's bubble up the error from the run function if the bind fails
     run(listener)?.await
 }
