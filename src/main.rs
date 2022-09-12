4408658e7535f9416d5359562bafa42920828361
Suresh Rangarajulu <surya501@yahoo.com>
2022-09-11 20:43:59 -0700

    feat: ✨ Added Logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -5,6 +6,10 @@ use base_proj::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // Before we do anything, let's setup the logger;
+    // We'll use the env_logger crate, which is a popular logging framework
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
