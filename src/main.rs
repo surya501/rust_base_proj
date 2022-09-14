072860e5e1768602fc3fa855d010b818d6f16b6f
Suresh Rangarajulu <surya501@yahoo.com>
2022-09-14 11:13:07 -0700

    feat: 🔊 Ensure that the span id is shown on all logs and formatted as json for easy consumption

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 use base_proj::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Before we do anything, let's setup the logger;
-    // We'll use the env_logger crate, which is a popular logging framework
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // configuration for the tracing capabilities
+
+    // First capture all the logs to the tracing.
+    LogTracer::init().expect("Failed to set logger.");
+    // Check for RUST_LOG env variable and if not set use the info as default.
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    // Let's send the output to stdout; later change this.
+    let formatting_layer = BunyanFormattingLayer::new("base_proj".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber");
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
