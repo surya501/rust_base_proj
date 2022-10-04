957f387cddff0f5dfcf04ef60db65fd217d7c4c8
Suresh Rangarajulu <surya501@yahoo.com>
2022-10-03 17:50:23 -0700

    fix: 🚧 add parameter import to newsletter to send data by post

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,7 +1,17 @@
+use actix_web::{web, HttpResponse};
 
-use actix_web::HttpResponse;
-// Dummy implementation
-pub async fn publish_newsletter() -> HttpResponse {
-HttpResponse::Ok().finish()
+#[derive(serde::Deserialize)]
+pub struct BodyData {
+    title: String,
+    content: Content,
+}
+#[derive(serde::Deserialize)]
+pub struct Content {
+    html: String,
+    text: String,
 }
 
+// Dummy implementation
+pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
