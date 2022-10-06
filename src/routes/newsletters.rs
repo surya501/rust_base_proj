de2d159e590449f70acc0dd295e8c92043bab241
Suresh Rangarajulu <surya501@yahoo.com>
2022-10-06 12:56:22 -0700

    feat: 🎨 move biz code to biz section

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -33,7 +33,7 @@ struct ConfirmedSubscriber {
 #[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
 async fn get_confirmed_subscribers(
     pool: &PgPool,
-) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
+) -> Result<Vec<Result<ConfirmedSubscriber, anyhow::Error>>, anyhow::Error> {
     struct Row {
         email: String,
     }
@@ -52,15 +52,9 @@ async fn get_confirmed_subscribers(
     // Map into the domain type
     let confirmed_subscribers = rows
         .into_iter()
-        .filter_map(|r| match SubscriberEmail::parse(r.email) {
-            Ok(email) => Some(ConfirmedSubscriber { email }),
-            Err(error) => {
-                tracing::warn!(
-                    "A confirmed subscriber is using an invalid email address.\n{}.",
-                    error
-                );
-                None
-            }
+        .map(|r| match SubscriberEmail::parse(r.email) {
+            Ok(email) => Ok(ConfirmedSubscriber { email }),
+            Err(error) => Err(anyhow::anyhow!(error)),
         })
         .collect();
 
@@ -87,17 +81,32 @@ pub async fn publish_newsletter(
     let subscribers = get_confirmed_subscribers(&pool).await?;
 
     for subscriber in subscribers {
-        email_client
-            .send_email(
-                &subscriber.email,
-                &body.title,
-                &body.content.html,
-                &body.content.text,
-            )
-            .await
-            .with_context(|| {
-                format!("Failed to send newsletter issue to {:?}", subscriber.email)
-            })?;
+        match subscriber {
+            Ok(subscriber) => {
+                email_client
+                    .send_email(
+                        &subscriber.email,
+                        &body.title,
+                        &body.content.html,
+                        &body.content.text,
+                    )
+                    .await
+                    .with_context(|| {
+                        format!("Failed to send newsletter issue to {}", subscriber.email)
+                    })?;
+            }
+            Err(error) => {
+                tracing::warn!(
+                // We record the error chain as a structured field
+                // on the log record.
+                error.cause_chain = ?error,
+                // Using `\` to split a long string literal over
+                // two lines, without creating a `\n` character.
+                "Skipping a confirmed subscriber. \
+                Their stored contact details are invalid",
+                );
+            }
+        }
     }
 
     Ok(HttpResponse::Ok().finish())
